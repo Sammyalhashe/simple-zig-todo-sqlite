672c935444ebecf3b1c8e20e64f445424f0dd04d
Sammyalhashe <sammy@salh.xyz>
2026-05-09 09:07:39 -0400

    [build] Adding build options to pass remote/password in.

--- build.zig
+++ build.zig
@@ -30,4 +30,19 @@ pub fn build(b: *std.Build) void {
     exe.root_module.linkSystemLibrary("mysqlclient", .{});
 
     b.installArtifact(exe);
+
+    // Convenience run step
+    const run_exe = b.addRunArtifact(exe);
+    run_exe.addArgs(&.{ "-r", b.option([] const u8, "r", "remote where mariadb instance is hosted") orelse "oldboy.salh.xyz" });
+
+    if (b.option([]const u8, "p", "password to the remote")) |password| {
+        run_exe.addArgs(&.{ "-p", password });
+    }
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
+
+    if (b.args) |args| {
+        run_exe.addArgs(args);
+    }
+
 }
